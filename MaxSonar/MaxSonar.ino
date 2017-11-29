741353bc9f1fbe92efc14a03a5237dccac661837
nxu8@jhu.edu <nxu8@jhu.edu>
2017-11-29 17:53:20 -0500

    Ultrasound MaxBotix ROS Package

--- MaxSonar/MaxSonar.ino
+++ MaxSonar/MaxSonar.ino
@@ -1,5 +1,6 @@
 /*
     * PW is digital pin 8
+    * PW1 is digital pin 7
     * TX is digital pin 6
     * AN is analog pin A0
  */
@@ -15,19 +16,15 @@
 Servo myServo;
 //
 Maxbotix rangeSensorPW(8, Maxbotix::PW, Maxbotix::LV);
-
+Maxbotix rangeSensorPW_s(7, Maxbotix::PW, Maxbotix::LV);
 Maxbotix rangeSensorAD(A0, Maxbotix::AN, Maxbotix::LV);
        
-//#define echoPin 7
-//#define trigPin 8
-//#define echoPin1 4
-//#define trigPin1 2
 
 // variables will change:
 int lockerState = 0;         
 float object_distance;
 //int object_distance1;
-int motor_angle;
+
 long duration, distance;
 long duration1, distance1;
 
@@ -38,16 +35,15 @@ ros::NodeHandle  nh;
 sensor_msgs::Range range_msg;
 ros::Publisher pub_range( "/ultrasound", &range_msg);
 char frameid[] = "/ultrasound1";
-
+char frameid1[] = "/ultrasound2";
 void setup()
 {
   nh.initNode();
 //  nh.advertise(sonar);
   nh.advertise(pub_range);
   Serial.begin(9600);
-  myServo.attach(12); //Servo trigger PIN
   range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  range_msg.header.frame_id =  frameid;
+
   range_msg.field_of_view = 0.1;  // fake
   range_msg.min_range = 17;
   range_msg.max_range = 300;
@@ -55,28 +51,18 @@ void setup()
 
 void loop()
 {
-  Servo_with_sonar();
-//   object_distance=rangeSensorPW.getRange();
-//   object_distance1=calculateDistance1();
-//  str_msg1.data = object_distance;
-//  sonar.publish( &str_msg1 );
+
     range_msg.range = rangeSensorPW.getRange();
+    range_msg.header.frame_id =  frameid;
     range_msg.header.stamp = nh.now();
     pub_range.publish(&range_msg);
-
+    //
+    range_msg.header.frame_id =  frameid1;
+    range_msg.range = rangeSensorPW_s.getRange();
+    range_msg.header.stamp = nh.now();
+    pub_range.publish(&range_msg);
   nh.spinOnce();
   delay(100);
 }
 
 
-//*****Servo motor with sonar
-void Servo_with_sonar(){
-  for(int i=15;i<=165;i=i+10){  
-      myServo.write(i);
-      delay(30);
-   }
-   for(int i=165;i>15;i=i-10){  
-      myServo.write(i);
-      delay(30);
-   } 
-}
